c26dd045af5843155f66fadbd347052ef03d5d1f
Atreya Choudhury <atreyachoudhury@hotmail.com>
2024-12-14 14:14:42 +0100

    add homepage

--- Formula/yamet.rb
+++ Formula/yamet.rb
@@ -1,5 +1,6 @@
 class Yamet < Formula
-  desc "Yamet: Yet Another Methylation Entropy Tool" 
+  desc "Yamet: Yet Another Methylation Entropy Tool"
+  homepage "https://github.com/imallona/yamet"
   url "https://github.com/imallona/yamet/releases/download/v1.0.0-dev/yamet-v1.0.0-dev.tar.gz"
   sha256 "f91801f5151a32b5547a7070b76b93d6e9178422310f28e5584d9682b1471191"
   license "GPL"
