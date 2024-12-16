db75b061345ff4b08619ed5bf0b06e1f06326697
Atreya Choudhury <atreyachoudhury@hotmail.com>
2024-12-16 15:43:21 +0100

    update checksum and license

--- Formula/yamet.rb
+++ Formula/yamet.rb
@@ -2,8 +2,8 @@ class Yamet < Formula
   desc "Yamet: Yet Another Methylation Entropy Tool"
   homepage "https://github.com/imallona/yamet"
   url "https://github.com/imallona/yamet/releases/download/v1.0.0-dev/yamet-v1.0.0-dev.tar.gz"
-  sha256 "f91801f5151a32b5547a7070b76b93d6e9178422310f28e5584d9682b1471191"
-  license "GPL"
+  sha256 "8f0c2cb393876b576cfa5ce67e62fd8a4708dd267481317dfd9b3cccac73d6f9"
+  license "GPL-3.0-only"
 
   depends_on "boost"
   depends_on "zlib"
