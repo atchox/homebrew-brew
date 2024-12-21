d67e19d990ff36142036d50b2583d93d5182e370
Atreya Choudhury <atreyachoudhury@hotmail.com>
2024-12-21 20:33:03 +0100

    update yamet formula url

--- Formula/yamet.rb
+++ Formula/yamet.rb
@@ -1,8 +1,8 @@
 class Yamet < Formula
   desc "Yamet: Yet Another Methylation Entropy Tool"
   homepage "https://github.com/imallona/yamet"
-  url "https://github.com/imallona/yamet/releases/download/v1.0.0-dev/yamet-v1.0.0-dev-Darwin-arm64.tar.gz"
-  sha256 "5b1450324789d6b1ffec4748e531d5fb72db489ef3a9415c93cf46258d57e7fc"
+  url "https://github.com/imallona/yamet/releases/download/v1.0.0-dev/yamet-Darwin-arm64-v1.0.0-dev.tar.gz"
+  sha256 "a003571b12404142955f9a21542c636fecb3bd27ea6bac2793ac71a5c166f584"
   license "GPL-3.0-only"
 
   depends_on "boost"
