ae56ab924e5c683a6d1a3255b5ce4b625b20e1e3
Atreya Choudhury <atreyachoudhury@hotmail.com>
2025-01-17 20:45:01 +0100

    update formula for proper bottling

--- Formula/yamet.rb
+++ Formula/yamet.rb
@@ -1,20 +1,29 @@
 class Yamet < Formula
-  desc "Yamet: Yet Another Methylation Entropy Tool"
+  desc "Yet Another Methylation Entropy Tool"
   homepage "https://github.com/imallona/yamet"
-  url "https://github.com/imallona/yamet/releases/download/v1.0.0-dev/yamet-Darwin-arm64-v1.0.0-dev.tar.gz"
-  sha256 "a003571b12404142955f9a21542c636fecb3bd27ea6bac2793ac71a5c166f584"
+  url "https://github.com/imallona/yamet/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "42ab8d7552751552e6ad344d5128aaeadf7c410f1f7f9ca06d078e365cabbe24"
   license "GPL-3.0-only"
 
+  depends_on "cmake" => :build
   depends_on "boost"
-  depends_on "zlib"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib"
+  end
 
   def install
-    bin.install "bin/yamet"
-    include.install Dir["include/*"]
+    rm_r Dir["{README.md,CHANGELOG.md,AUTHORS,LICENSE}"]
+    cd "method" do
+      system "cmake", ".", *std_cmake_args, "-DVERSION=#{version}"
+      system "cmake", "--build", "."
+      system "cmake", "--install", "."
+    end
   end
 
   test do
     # Verify installation
-    assert_match "Usage:", shell_output("#{bin}/yamet --help")
+    assert_match "yamet version #{version}", shell_output("#{bin}/yamet --version")
   end
 end
