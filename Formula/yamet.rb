e2c33749a01c88e182d9bfd555eb98e10e67065d
Atreya Choudhury <atreyachoudhury@hotmail.com>
2025-06-24 16:48:05 +0200

    update source url and sha + dependencies

--- Formula/yamet.rb
+++ Formula/yamet.rb
@@ -1,25 +1,20 @@
 class Yamet < Formula
   desc "Yet Another Methylation Entropy Tool"
   homepage "https://github.com/imallona/yamet"
-  url "https://github.com/imallona/yamet/archive/refs/tags/v1.1.0-rc.4.tar.gz"
-  sha256 "a2144b606a19a5f5bec01cd3000add5f78f313ad62e9a8cbe1949dc44b5a8dcb"
+  url "https://github.com/imallona/yamet/archive/refs/tags/v1.1.0-rc.5.tar.gz"
+  sha256 "e5620827e1c48b35724d97f93a4259de415ccc7320130a0762a70727ec1c1aea"
   license "GPL-3.0-only"
   head "https://github.com/imallona/yamet.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/imallona/yamet/releases/download/v1.1.0-rc.4"
+    root_url "https://github.com/imallona/yamet/releases/download/v1.1.0-rc.5"
     sha256 cellar: :any,                 arm64_sequoia: "b50e5ca42023d368aca9fee81717f08df644520a036dc3532f86192d197ec1d8"
     sha256 cellar: :any,                 arm64_sonoma:  "c5a32baa9969c87b39e241d082a398276a14a4e995982cbfe12da4a1a996d29a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "e407540d97220b46c129e96ad89682af04a7ac4daa49565d9f0d9baa5ba09517"
   end
 
   depends_on "cmake" => :build
-  depends_on "boost" => :build
-  uses_from_macos "zlib"
-
-  on_linux do
-    depends_on "zlib"
-  end
+  depends_on "boost"
 
   def install
     rm_r Dir["{README.md,CHANGELOG.md,AUTHORS,LICENSE}"]
